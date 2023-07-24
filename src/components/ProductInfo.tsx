d82cfe784413d2f52acf38ea21579d88521a1d75
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-25 01:48:30 +0200

    add project info

---
+++ src/components/ProductInfo.tsx
@@ -0,0 +1,48 @@
+import {Product} from "@/types/product";
+import Currency from "@/components/ui/Currency";
+import Button from "@/components/ui/Button";
+import {ShoppingCart} from "lucide-react";
+
+interface ProductInfoProps {
+    data: Product;
+}
+
+const ProductInfo = ({data}: ProductInfoProps) => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+
+            <div className="mt-3 flex items-end justify-between">
+                <p className="text-2xl text-gray-900">
+                    <Currency value={data.price} />
+                </p>
+            </div>
+
+            <hr className="my-4"/>
+
+            <div className="flex flex-col gap-y-6">
+                <div className="flex items-center gap-x-4">
+                    <h3 className="font-semibold text-black">Size:</h3>
+                    <div>{data.size.name}</div>
+                </div>
+
+                <div className="flex items-center gap-x-4">
+                    <h3 className="font-semibold text-black">Color:</h3>
+                    <div
+                        className="h-6 w-6 rounded-full border border-gray-600"
+                        style={{backgroundColor: data.color.value}}
+                    />
+                </div>
+            </div>
+
+            <div className="mt-10 flex items-center gap-x-3">
+                <Button className="flex items-center gap-x-2">
+                    Add to Cart
+                    <ShoppingCart />
+                </Button>
+            </div>
+        </div>
+    )
+};
+
+export default ProductInfo;
