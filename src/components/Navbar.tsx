e9f60cf0b40127fb33b39c5678b637b99bde7d14
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-07-24 17:00:31 +0200

    add nav action button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/ui/container";
 import Link from "next/link";
 import MainNav from "@/components/MainNav";
 import getCategories from "@/lib/actions/get-categories";
+import NavbarActions from "@/components/NavbarActions";
 
 export const revalidate = 0;
 
@@ -15,6 +16,7 @@ const Navbar = async () => {
                         <p className="font-bold text-xl">STORE</p>
                     </Link>
                     <MainNav data={categories} />
+                    <NavbarActions/>
                 </div>
             </Container>
         </div>
